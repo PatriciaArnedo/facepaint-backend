94580d1f8235b095afc7f08f4cb918381588bff1
PatriciaArnedo <paaarnedo@gmail.com>
2021-01-22 21:32:12 -0500

    added function in user model to get save count

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,10 @@ class User < ApplicationRecord
 
     has_many :filters
     has_many :save_filters
-    # has_many :savedfilters, :class_name => "Filter", through: :save_filters
     has_secure_password
 
+    def save_count
+        saved = SaveFilter.all.filter{ |filter| filter.id_user == self.id}
+        return saved.length()
+    end
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :avatar, :username, :name, :instagram, :bio
+  attributes :id, :avatar, :username, :name, :instagram, :bio, :save_count
   has_many :filters
   has_many :save_filters
 end
