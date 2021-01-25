b88e340c999201dc03f3bb4af6da385c6af230a5
PatriciaArnedo <paaarnedo@gmail.com>
2021-01-25 00:43:39 -0500

    productionizing continued

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,6 +14,7 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
 
   config.hosts << "facepaint-backend-mad3zevtda-ue.a.run.app"
+  config.hosts << "localhost"
 
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
@@ -22,7 +23,6 @@ Rails.application.configure do
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
-  config.assets.compile = true
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
